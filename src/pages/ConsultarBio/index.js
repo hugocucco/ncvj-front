d6f847652c68cde9657cfe2a139050ef7cf8d6a6
Hugo Cucco <hugo_cs22@hotmail.com>
2019-10-25 00:54:17 -0300

    fix tela de resultado e api biometria

--- src/pages/ConsultarBio/index.js
+++ src/pages/ConsultarBio/index.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import * as Yup from 'yup';
+// import * as Yup from 'yup';
 import { toast } from 'react-toastify';
 import api from '~/services/api';
 import biometria from '~/services/biometria';
 
 import { Container } from './styles';
 
-const schema = Yup.object().shape({
-  template1: Yup.string().required('Entre com a biometria'),
-});
+// const schema = Yup.object().shape({
+//   template1: Yup.string().required('Entre com a biometria'),
+// });
 
 export default function ConsultarBio() {
   const [result, setResult] = useState({
@@ -20,25 +20,23 @@ export default function ConsultarBio() {
     uf_pendencia: '',
   });
 
-  const [input, setInput] = useState('');
+  // const [input, setInput] = useState('');
 
-  async function returnBiometria() {
-    const response = await biometria.get('api/public/v1/captura/Capturar/1');
-    setInput(response.data);
-    console.tron.log(response.data);
-  }
+  // async function returnBiometria() {
+  //   const response = await biometria.get('api/public/v1/captura/Capturar/1');
+  //   setInput(response.data);
+  //   console.tron.log(response.data);
+  // }
 
   async function consultar() {
     try {
       const responseBack = await api.get('templates');
-      console.tron.log(responseBack.data);
       const digitais = responseBack.data;
 
-      const responseApi = await biometria.post('Verificar', {
-        digitais,
-      });
+      const responseApi = await biometria.post('Verificar', digitais);
       console.tron.log(responseApi.data);
       const resultado = responseApi.data;
+      console.tron.log(resultado);
 
       const pessoa = await api.post('consultabio', {
         resultado,
@@ -56,20 +54,21 @@ export default function ConsultarBio() {
       <header>
         <strong>Consultar por Biometria</strong>
       </header>
-      <h3> Aperte o botão para checar a Biometria e depois em consultar</h3>
-      <Form schema={schema} onSubmit={consultar}>
+      <h3> Aperte o botão para checar a Biometria</h3>
+      <Form initialData={result} onSubmit={consultar}>
         <hr />
 
-        <Input
-          name="template1"
-          placeholder="Template"
-          value={input}
-          autoComplete="off"
-        />
-        <hr />
-        <button type="button" onClick={returnBiometria}>
-          Checar
-        </button>
+        <h4>Nome:</h4>
+        <Input name="name" disabled />
+        <h4>CPF:</h4>
+        <Input name="cpf" disabled />
+        <h4>Estado de origem:</h4>
+        <Input name="uf_origem" disabled />
+        <h4>Pendência:</h4>
+        <Input name="pendencia" disabled />
+        <h4>Estado da pendência:</h4>
+        <Input name="uf_pendencia" disabled />
+
         <hr />
         <hr />
         <div>
--- src/services/biometria.js
+++ src/services/biometria.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const biometria = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'http://localhost:9000/api/public/v1/captura/',
 });
 
 export default biometria;
