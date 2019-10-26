de8147155289079ef1a86b5af2881bcacf890514
Hugo Cucco <hugo_cs22@hotmail.com>
2019-10-25 23:43:27 -0300

    removendo comentarios2

--- src/pages/ConsultarBio/index.js
+++ src/pages/ConsultarBio/index.js
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
-// import * as Yup from 'yup';
 import { toast } from 'react-toastify';
 import api from '~/services/api';
 import biometria from '~/services/biometria';
 
 import { Container } from './styles';
-
-// const schema = Yup.object().shape({
-//   template1: Yup.string().required('Entre com a biometria'),
-// });
 
 export default function ConsultarBio() {
   const [result, setResult] = useState({
@@ -19,14 +14,6 @@ export default function ConsultarBio() {
     pendencia: '',
     uf_pendencia: '',
   });
-
-  // const [input, setInput] = useState('');
-
-  // async function returnBiometria() {
-  //   const response = await biometria.get('api/public/v1/captura/Capturar/1');
-  //   setInput(response.data);
-  //   console.tron.log(response.data);
-  // }
 
   async function consultar() {
     try {
